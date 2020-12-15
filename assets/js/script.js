2d4e833d6f7d36895861d11e4b9687dd80cb23ea
Seth Tuman <Seth.Tuman@gmail.com>
2020-12-15 11:16:18 -0800

    update code for lesson two

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,11 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-var buttonE1 = document.querySelector("#save-task");
-var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
-}
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
 
-buttonE1.addEventListener("click", createTaskHandler); 
-
-
+buttonEl.addEventListener("click", createTaskHandler);
